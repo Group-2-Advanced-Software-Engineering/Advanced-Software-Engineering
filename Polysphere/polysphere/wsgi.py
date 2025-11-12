432bde62978fde96af7d9865df447ee86b2b3f5e
Bettaswamy <Bettaswamy@github.com>
2025-11-12 15:09:23 +0000

    renamed from task3 to polysphere

--- Polysphere/polysphere/wsgi.py
+++ Polysphere/polysphere/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Task3 project.
+WSGI config for polysphere project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Task3.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'polysphere.settings')
 
 application = get_wsgi_application()
